5af32094cf071037c45c042bfe6730f45302fdae
Rakesh_Morichauhan <rakesh.morichauhan@prisomtechnology.com>
2023-10-17 16:19:17 +0530

    final commit

--- bmi_calculator_new-master/android/app/src/main/kotlin/com/example/bmi_calculator_new/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.bmi_calculator_new
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- bmi_calculator_new-master/lib/input_page/input_page_styles.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import '../widget_utils.dart' show screenAwareSize;
-
-double appBarHeight(BuildContext context) {
-  return screenAwareSize(80.0, context) + MediaQuery.of(context).padding.top;
-}
